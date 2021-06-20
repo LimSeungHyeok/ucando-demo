91481dd96901be1e15cee08de3f06c5d80b9d4f9
Gabriel <lmsghk@gmail.com>
2021-06-20 21:52:43 +0900

    feat: vue youtube rollback

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from "./plugins/vuetify";
 import router from "./routers/router";
-import vueYoutube from 'vue-youtube';
 
 Vue.config.productionTip = false
 
@@ -10,5 +9,4 @@ new Vue({
   render: h => h(App),
   router,
   vuetify,
-  vueYoutube
 }).$mount('#app')
